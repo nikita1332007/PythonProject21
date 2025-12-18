46468d54bc443fa0789ec2f8da9b2fca6df5361c
nikita <nikilodion87@gmail.com>
2025-12-18 18:49:00 +0300

    users

--- myproject/lms/views.py
+++ myproject/lms/views.py
@@ -1,18 +1,29 @@
 from rest_framework import viewsets, generics
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
+from ..users.models import Payment
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import PaymentSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+
 class LessonListCreate(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'amount', 'date']
+    ordering_fields = ['date', 'amount']
