b0330a2929c1650a762350c4735ec7f45bd6388f
nikita <nikilodion87@gmail.com>
2025-12-21 14:58:57 +0300

    lms

--- myproject/lms/views.py
+++ myproject/lms/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from . import permissions
 from .models import Course, Lesson, CourseSubscription
 from .paginators import StandardResultsSetPagination
 from .permissions import IsModeratorOrReadOnly, IsOwnerOrModerator
 from .serializers import CourseSerializer, LessonSerializer
+from .services import stripe_service
 from ..users.models import Payment
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import PaymentSerializer
@@ -67,3 +69,76 @@ class CourseSubscriptionAPIView(APIView):
             message = 'Подписка добавлена'
 
         return Response({'message': message})
+
+
+class CreatePaymentView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        course_id = request.data.get('course_id')
+        if not course_id:
+            return Response({"error": "course_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        try:
+            course = Course.objects.get(id=course_id)
+        except Course.DoesNotExist:
+            return Response({"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+        product_resp = stripe_service.create_stripe_product(name=course.title, description=course.description or "")
+        if "error" in product_resp:
+            return Response({"error": product_resp["error"]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        price_cents = int(course.price * 100)
+        price_resp = stripe_service.create_stripe_price(product_id=product_resp.id, unit_amount=price_cents)
+        if "error" in price_resp:
+            return Response({"error": price_resp["error"]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        success_url = request.build_absolute_uri('/payments/success/')
+        cancel_url = request.build_absolute_uri('/payments/cancel/')
+
+        session_resp = stripe_service.create_checkout_session(price_id=price_resp.id,
+                                                              success_url=success_url,
+                                                              cancel_url=cancel_url)
+        if "error" in session_resp:
+            return Response({"error": session_resp["error"]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+        payment = Payment.objects.create(
+            user=request.user,
+            course=course,
+            stripe_product_id=product_resp.id,
+            stripe_price_id=price_resp.id,
+            stripe_session_id=session_resp.id,
+            amount=course.price,
+            status='pending',
+            payment_url=session_resp.url,
+        )
+
+        return Response({
+            "payment_id": payment.id,
+            "payment_url": session_resp.url,
+            "stripe_session_id": session_resp.id,
+        }, status=status.HTTP_201_CREATED)
+
+class PaymentStatusView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, payment_id):
+        try:
+            payment = Payment.objects.get(id=payment_id, user=request.user)
+        except Payment.DoesNotExist:
+            return Response({"error": "Payment not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        session_resp = stripe_service.retrieve_checkout_session(payment.stripe_session_id)
+        if "error" in session_resp:
+            return Response({"error": session_resp["error"]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+        return Response({
+            "payment_id": payment.id,
+            "status": session_resp.payment_status,
+            "amount_total": session_resp.amount_total,
+            "currency": session_resp.currency,
+        })
--- myproject/project/urls.py
+++ myproject/project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project project.
+URL configuration for project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,10 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.db import router
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from myproject.lms.views import LessonListCreate, LessonRetrieveUpdateDestroy
+from myproject.users.views import RegisterAPIView
 
-from myproject.lms.views import LessonListCreate, LessonRetrieveUpdateDestroy, RegisterAPIView
+schema_view = get_schema_view(
+   openapi.Info(
+      title="LMS API",
+      default_version='v1',
+      description="Документация API для проекта LMS",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +45,9 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('users.urls')),
     path('api/lms/', include('lms.urls')),
+    path('api/', include('lms.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
